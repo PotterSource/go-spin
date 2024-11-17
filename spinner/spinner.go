5a174aba4cbbee82aeacac90b81606def51eee2f
PotterSource <davidpotterstudio@gmail.com>
2024-11-17 03:03:34 -0600

    allow optional message to be passed into update with the spinner

--- spinner/spinner.go
+++ spinner/spinner.go
@@ -47,15 +47,20 @@ func NewSpinner(
 	}, nil
 }
 
-func (s *Spinner) Start() {
+func (s *Spinner) Start(messages ...string) {
 	go func() {
+		message := "" // Default to no message
+		if len(messages) > 0 {
+			message = " " + messages[0] // Add a space for separation, use only the first message if provided
+		}
 		for {
 			for _, frame := range s.frames {
 				select {
 				case <-s.stop:
+					fmt.Printf("\r\033[K")
 					return
 				default:
-					fmt.Printf("\r%s%s\033[0m", s.color.ANSI(), frame)
+					fmt.Printf("\r%s%s%s\033[0m", s.color.ANSI(), frame, message)
 					time.Sleep(100 * time.Millisecond)
 				}
 			}
